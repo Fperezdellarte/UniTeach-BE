fb981fef84debd1bc8533b449323d1d46ddd44f4
Fperezdellarte <fperezdellarte@gmail.com>
2024-06-25 20:11:20 -0300

    dependencias de la base de datos

--- config/mysql.js
+++ config/mysql.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const dbConnect = () => {
@@ -6,7 +6,9 @@ const dbConnect = () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        database: process.env.DB_DATABASE,
+        connectTimeout: 60000,
+        port: process.env.DB_PORT
     });
 
     connection.connect((err) => {
