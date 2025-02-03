a3141371d8303b06dd806315cd71a4589646764d
Alvaro <alvaro.ybanez16@gmail.com>
2025-02-03 00:09:10 -0300

    mejoras hechas en clases y buscador

--- app/models/classesModels.js
+++ app/models/classesModels.js
@@ -1,174 +1,197 @@
-const { dbConnect } = require('../../config/mysql');
-const { calculateEndDate } = require('../services/classesService');
+const { dbConnect } = require("../../config/mysql");
+const { calculateEndDate } = require("../services/classesService");
 
 const createClass = async (classData) => {
-    const connection = await dbConnect().promise();
+  const connection = await dbConnect().promise();
 
-    // Calcular la fecha y hora de fin de la clase
-    const endDate = calculateEndDate(classData.hour, classData.date);
+  // Calcular la fecha y hora de fin de la clase
+  const endDate = calculateEndDate(classData.hour, classData.date);
 
-    // Determinar si la clase ha expirado
-    const expired = new Date(endDate) < new Date() ? true : false;
+  // Determinar si la clase ha expirado
+  const expired = new Date(endDate) < new Date() ? true : false;
 
-    const query = "INSERT INTO classes (hour, date, Place, Subjects_idSubjects, Users_idCreator, endDate, expired) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    const values = [
-        classData.hour,
-        classData.date,
-        classData.Place,
-        classData.Subjects_idSubjects,
-        classData.Users_idCreator,
-        endDate, // Fecha y hora de fin de clase
-        expired // Indica si la clase ha expirado
-    ];
+  const query =
+    "INSERT INTO classes (hour, date, Place, Subjects_idSubjects, Users_idCreator, endDate, expired) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  const values = [
+    classData.hour,
+    classData.date,
+    classData.Place,
+    classData.Subjects_idSubjects,
+    classData.Users_idCreator,
+    endDate, // Fecha y hora de fin de clase
+    expired, // Indica si la clase ha expirado
+  ];
 
-    try {
-        const [result] = await connection.query(query, values);
-        return result;
-    } catch (err) {
-        throw err;
-    } finally {
-        await connection.end();
-    }
+  try {
+    const [result] = await connection.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    await connection.end();
+  }
 };
 
-
 const getAllClasses = async () => {
-    const connection = await dbConnect().promise();
+  const connection = await dbConnect().promise();
 
-    const query = "SELECT * FROM classes";
+  const query = "SELECT * FROM classes";
 
-    try {
-        const [results] = await connection.query(query);
-        return results;
-    } catch (err) 
-    {
-        throw err;
-    }
-    finally{
-
-      await  connection.end();
-    }
-}
+  try {
+    const [results] = await connection.query(query);
+    return results;
+  } catch (err) {
+    throw err;
+  } finally {
+    await connection.end();
+  }
+};
 
 const getClassById = async (classId) => {
-    const connection = await dbConnect().promise();
-    const query = "SELECT * FROM classes WHERE idClasses = ?";
-    try {
-        const [results] = await connection.query(query, [classId]);
-        console.log('clase obtenida correctamente');
-        return results.length > 0 ? results[0] : null
-    } catch (err) 
-    {
-      throw err;  
-    }
-    finally{
-        await connection.end();
-    }
-}
+  const connection = await dbConnect().promise();
+  const query = `
+        SELECT 
+            c.*, 
+            s.Name AS subjectName, 
+            u.Name AS creatorName
+        FROM 
+            classes c
+        JOIN 
+            subjects s ON c.Subjects_idSubjects = s.idSubjects
+        JOIN 
+            users u ON c.Users_idCreator = u.idUser
+        WHERE 
+            c.idClasses = ?
+    `;
+  try {
+    const [results] = await connection.query(query, [classId]);
+    console.log("clase obtenida correctamente");
+    return results.length > 0 ? results[0] : null;
+  } catch (err) {
+    throw err;
+  } finally {
+    await connection.end();
+  }
+};
 
 const modifyClass = async (classId, classData) => {
-    const connection = await dbConnect().promise();
+  const connection = await dbConnect().promise();
 
-    // Primero, obtener todos los datos actuales necesarios
-    const [currentClass] = await connection.query("SELECT Date, hour FROM classes WHERE idClasses = ?", [classId]);
+  // Primero, obtener todos los datos actuales necesarios
+  const [currentClass] = await connection.query(
+    "SELECT Date, hour FROM classes WHERE idClasses = ?",
+    [classId]
+  );
 
-    const currentDate = currentClass[0]?.Date;
-    const currentHour = currentClass[0]?.hour;
-   
-    if (!currentDate || !currentHour) {
-        throw new Error('Fecha de inicio y hora actuales no encontradas para calcular la fecha de fin.');
-    }
+  const currentDate = currentClass[0]?.Date;
+  const currentHour = currentClass[0]?.hour;
 
-    // Verificar qué campos se están actualizando
-    const fields = Object.keys(classData);
-    const updateDate = fields.includes('Date');
-    const updateHour = fields.includes('hour');
+  if (!currentDate || !currentHour) {
+    throw new Error(
+      "Fecha de inicio y hora actuales no encontradas para calcular la fecha de fin."
+    );
+  }
 
-    let endDate;
+  // Verificar qué campos se están actualizando
+  const fields = Object.keys(classData);
+  const updateDate = fields.includes("Date");
+  const updateHour = fields.includes("hour");
 
-    // Calcular la nueva fecha de fin
-    if (updateDate && updateHour) {
-        endDate = calculateEndDate(classData.hour, classData.Date);
-    } else if (updateHour) {
-        endDate = calculateEndDate(classData.hour, currentDate);
-    } else if (updateDate) {
-        endDate = calculateEndDate(currentHour, classData.Date);
-    } else {
-        // Si no se actualiza ni fecha ni hora, mantener la fecha de fin actual
-        endDate = currentClass[0]?.endDate;
-    }
+  let endDate;
 
-    // Construir la consulta para la actualización
-    let query = "UPDATE classes SET ";
-    const setValues = fields.map(field => `${field} = ?`).join(', ');
-    query += setValues + ", endDate = ? WHERE idClasses = ?";
+  // Calcular la nueva fecha de fin
+  if (updateDate && updateHour) {
+    endDate = calculateEndDate(classData.hour, classData.Date);
+  } else if (updateHour) {
+    endDate = calculateEndDate(classData.hour, currentDate);
+  } else if (updateDate) {
+    endDate = calculateEndDate(currentHour, classData.Date);
+  } else {
+    // Si no se actualiza ni fecha ni hora, mantener la fecha de fin actual
+    endDate = currentClass[0]?.endDate;
+  }
 
-    // Construir los valores para la consulta
-    const values = fields.map(field => classData[field]);
-    values.push(endDate);
-    values.push(classId);
+  // Construir la consulta para la actualización
+  let query = "UPDATE classes SET ";
+  const setValues = fields.map((field) => `${field} = ?`).join(", ");
+  query += setValues + ", endDate = ? WHERE idClasses = ?";
 
-    try {
-        const [result] = await connection.query(query, values);
-        return result;
-    } catch (err) {
-        throw err;
-    } finally {
-        await connection.end();
-    }
+  // Construir los valores para la consulta
+  const values = fields.map((field) => classData[field]);
+  values.push(endDate);
+  values.push(classId);
+
+  try {
+    const [result] = await connection.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    await connection.end();
+  }
 };
 
 const getAllClassesOfMentor = async (idUser, subject) => {
-    const connection = await dbConnect().promise();
-    try {
-        // Obtener todas las clases del mentor
-        const [classes] = await connection.query("SELECT * FROM classes WHERE Users_idCreator = ?", [idUser]);
-        
-        if (classes.length > 0) {
-            console.log("Clases obtenidas correctamente");
+  const connection = await dbConnect().promise();
+  try {
+    // Obtener todas las clases del mentor
+    const [classes] = await connection.query(
+      "SELECT * FROM classes WHERE Users_idCreator = ?",
+      [idUser]
+    );
 
-            // Iterar sobre cada clase para obtener el nombre de la materia
-            for (let i = 0; i < classes.length; i++) {
-                const classItem = classes[i];
-                const [subjectResult] = await connection.query("SELECT Name FROM subjects WHERE idSubjects = ?", [classItem.Subjects_idSubjects]);
-                
-                if (subjectResult.length > 0) {
-                    // Reemplazar el ID de la materia con el nombre
-                    classItem.SubjectName = subjectResult[0].Name;
-                } else {
-                    classItem.SubjectName = "Materia no encontrada";
-                }
-            }
+    if (classes.length > 0) {
+      console.log("Clases obtenidas correctamente");
 
-            return classes;
+      // Iterar sobre cada clase para obtener el nombre de la materia
+      for (let i = 0; i < classes.length; i++) {
+        const classItem = classes[i];
+        const [subjectResult] = await connection.query(
+          "SELECT Name FROM subjects WHERE idSubjects = ?",
+          [classItem.Subjects_idSubjects]
+        );
+
+        if (subjectResult.length > 0) {
+          // Reemplazar el ID de la materia con el nombre
+          classItem.SubjectName = subjectResult[0].Name;
         } else {
-            console.log("No se encontró ninguna clase");
-            return null;
+          classItem.SubjectName = "Materia no encontrada";
         }
-    } catch (err) {
-        console.error("Error al obtener las clases:", err);
-        throw err;
-    } finally {
-        await connection.end();
+      }
+
+      return classes;
+    } else {
+      console.log("No se encontró ninguna clase");
+      return null;
     }
+  } catch (err) {
+    console.error("Error al obtener las clases:", err);
+    throw err;
+  } finally {
+    await connection.end();
+  }
 };
 
-
-
-
 const deleteClass = async (classId) => {
-    const connection = await dbConnect().promise();
+  const connection = await dbConnect().promise();
 
-    const query = "DELETE FROM classes WHERE idClasses = ?";
+  const query = "DELETE FROM classes WHERE idClasses = ?";
 
-    try {
-        const [result] = await connection.query(query, [classId]);
-        return result;
-    } catch (err) {
-        throw err;
-    } finally {
-        await connection.end();
-    }
-}
+  try {
+    const [result] = await connection.query(query, [classId]);
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    await connection.end();
+  }
+};
 
-module.exports = { getAllClasses, getClassById, createClass, modifyClass, deleteClass, getAllClassesOfMentor };
+module.exports = {
+  getAllClasses,
+  getClassById,
+  createClass,
+  modifyClass,
+  deleteClass,
+  getAllClassesOfMentor,
+};
